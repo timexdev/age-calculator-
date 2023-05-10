105eba2224537df48526eb8902c296e2173c5e1d
Peter Ajala <ajalapeter1@gmail.com>
2023-05-09 23:19:07 -0700

    worked on functionality

--- script.js
+++ script.js
@@ -24,32 +24,3 @@ function calculateAge(){
 }
 
 
-// const calculateAge = () => {
-//     const yob = document.getElementById("year").value;
-//     const mob = document.getElementById("month").value;
-//     const dob = document.getElementById("day").value;
-
-//     const now = new Date();
-//     const birthDate = new Date(yob, mob - 1, dob);
-//     let ageYears = now.getFullYear() - birthDate.getFullYear();
-//     let ageMonths = now.getMonth() - birthDate.getMonth();
-//     let ageDays = now.getDate() - birthDate.getDate();
-
-//     const birthdayThisYear = new Date(now.getFullYear(), birthDate.getMonth(), birthDate.getDate());
-//     if (now < birthdayThisYear) {
-//       ageMonths--;
-//       ageDays = now.getDate() + (new Date(now.getFullYear(), now.getMonth() - 1, 0).getDate()) - birthDate.getDate();
-//       if (ageDays >= 30) {
-//         ageMonths++;
-//         ageDays -= 30;
-//       }
-//     }
-//     document.getElementById('display-year').innerHTML = ageYears
-//     document.getElementById('display-month').innerHTML = ageMonths
-//     document.getElementById('display-day').innerHTML = ageDays
-
-
-
-    // const ageElement = document.getElementById("age");
-//     // ageElement.textContent = `You are ${ageYears} years, ${ageMonths} months, and ${ageDays} days old.`;
-//   }
